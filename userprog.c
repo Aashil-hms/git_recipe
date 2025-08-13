14532282f2806527b2fd03c5e3a35d167be19336
Aashil-hms <aashil@hms.co.in>
2025-08-13 07:09:27 +0000

    updated source

--- userprog.c
+++ userprog.c
@@ -2,10 +2,6 @@
 
 int main()
 {
-	#ifdef USE_SYSCALL
-		write(1, "Hello System Call", 17);
-	#else
-		printf("Hello World");
-	#endif
-	return 0;
+      printf("Hello LWL Team latest \n");
+      return 0;
 }
